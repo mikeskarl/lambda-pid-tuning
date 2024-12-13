4e806c0937bc99a44520b464032751a2840b0273
mikeskarl <141687998+mikeskarl@users.noreply.github.com>
2024-12-13 10:58:55 -0800

    Update root page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import PIDTuningApp from '../components/PIDTuningApp';
+import PIDTuningApp from '../components/PIDTuningApp'
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen p-4">
       <PIDTuningApp />
     </main>
-  );
+  )
 }
